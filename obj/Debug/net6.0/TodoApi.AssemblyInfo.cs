a600c72472f2102a532fe86bf5e87c9a1abd7459
tajbaevshamsiddin <tajbaevsh@gmail.com>
2024-03-04 17:31:12 +0500

    qwe

--- obj/Debug/net6.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd541500000985b1154e33436ea01e9df62a896f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eedc04fde5372655d399b15dbbb5b007887042e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
